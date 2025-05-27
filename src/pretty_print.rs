91906c71715e827f762ab2c5ac8c45ad5e0b01ca
jokemanfire <hu.dingyang@zte.com.cn>
2025-05-27 09:13:28 +0800

    change to use comfy_table
    Signed-off-by: jokemanfire <hu.dingyang@zte.com.cn>

--- src/pretty_print.rs
+++ src/pretty_print.rs
@@ -1,4 +1,4 @@
-use prettytable::{row, Table};
+use comfy_table::{Cell, Table};
 
 use crate::{KvInfo, TableInfo};
 
@@ -9,25 +9,29 @@ pub trait PrintTable {
 impl PrintTable for TableInfo {
     fn print_data(&self) {
         let mut table = Table::new();
-        table.add_row(row![Fgb=>"id", "name"]);
+        table.set_header(vec![Cell::new("id"), Cell::new("name")]);
         let mut cnt = 1;
         self.tablename.iter().for_each(|t| {
-            table.add_row(row![cnt.to_string(), &t]);
+            table.add_row(vec![Cell::new(cnt.to_string()), Cell::new(t)]);
             cnt += 1
         });
-        table.printstd();
+        println!("{table}");
     }
 }
 
 impl PrintTable for KvInfo {
     fn print_data(&self) {
         let mut table = Table::new();
-        table.add_row(row![Fgb=>"id", "key", "value"]);
+        table
+            .load_preset(comfy_table::presets::UTF8_FULL)
+            .apply_modifier(comfy_table::modifiers::UTF8_ROUND_CORNERS)
+            .set_content_arrangement(comfy_table::ContentArrangement::Dynamic)
+            .set_header(vec![Cell::new("id"), Cell::new("key"), Cell::new("value")]);
         let mut cnt = 1;
         self.kvdatas.iter().for_each(|(k, v)| {
-            table.add_row(row![cnt.to_string(), k, v]);
+            table.add_row(vec![Cell::new(cnt.to_string()), Cell::new(k), Cell::new(v)]);
             cnt += 1
         });
-        table.printstd();
+        println!("{table}");
     }
 }
